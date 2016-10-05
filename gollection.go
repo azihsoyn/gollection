00602fb6610d02539c04a5baef641614f1a508ff
azihsoyn <azihsoyn@gmail.com>
2016-10-06 02:04:33 +0900

    implement Fold

--- gollection.go
+++ gollection.go
@@ -195,6 +195,36 @@ func (g *gollection) Reduce(f func(v1, v2 interface{}) interface{}) *gollection 
 	}
 }
 
+func (g *gollection) Fold(v0 interface{}, f func(v1, v2 interface{}) interface{}) *gollection {
+	if g.err != nil {
+		return &gollection{err: g.err}
+	}
+
+	sv := reflect.ValueOf(g.slice)
+	if sv.Kind() != reflect.Slice {
+		return &gollection{
+			slice: nil,
+			err:   fmt.Errorf("gollection.Reduce called with non-slice value of type %T", g.slice),
+		}
+	}
+
+	if sv.Len() < 1 {
+		return &gollection{
+			val: v0,
+		}
+	}
+
+	v1 := v0
+	for i := 0; i < sv.Len(); i++ {
+		v2 := sv.Index(i).Interface()
+		v1 = f(v1, v2)
+	}
+
+	return &gollection{
+		val: v1,
+	}
+}
+
 func (g *gollection) Result() (interface{}, error) {
 	if g.val != nil {
 		return g.val, g.err
