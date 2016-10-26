ed64da02ada428e6d5c8f0e0141ef5deb4388e70
azihsoyn <azihsoyn@gmail.com>
2016-10-26 17:54:12 +0900

    add reduce and fold test

--- reduce_test.go
+++ reduce_test.go
@@ -1,1 +1,26 @@
-package gollection
+package gollection_test
+
+import (
+	"testing"
+
+	"github.com/azihsoyn/gollection"
+	"github.com/stretchr/testify/assert"
+)
+
+func ReduceTest(t *testing.T) {
+	assert := assert.New(t)
+
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expect := 55
+
+	res, err := gollection.New(arr).Reduce(func(v1, v2 interface{}) interface{} {
+		n1, ok1 := v1.(int)
+		n2, ok2 := v2.(int)
+		if ok1 && ok2 {
+			return n1 + n2
+		}
+		return ""
+	}).Result()
+	assert.NoError(err)
+	assert.Equal(expect, res)
+}
