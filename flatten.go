8256c37e0582b0ae8d478d0a36f119ab05df5555
azihsoyn <azihsoyn@gmail.com>
2016-10-26 17:31:35 +0900

    add flatten test

--- flatten.go
+++ flatten.go
@@ -14,24 +14,28 @@ func (g *gollection) Flatten() *gollection {
 	if sv.Kind() != reflect.Slice {
 		return &gollection{
 			slice: nil,
-			err:   fmt.Errorf("gollection.FlatMap called with non-slice value of type %T", g.slice),
+			err:   fmt.Errorf("gollection.Flatten called with non-slice value of type %T", g.slice),
 		}
 	}
 
-	ret := make([]interface{}, 0, sv.Len())
+	var ret reflect.Value
 	for i := 0; i < sv.Len(); i++ {
 		v := sv.Index(i).Interface()
 		svv := reflect.ValueOf(v)
+		// init
+		if i == 0 {
+			ret = reflect.MakeSlice(svv.Type(), 0, sv.Len())
+		}
 		if svv.Kind() != reflect.Slice {
 			continue
 		}
 		for j := 0; j < svv.Len(); j++ {
-			ret = append(ret, svv.Index(j).Interface())
+			ret = reflect.Append(ret, svv.Index(j))
 		}
 	}
 
 	return &gollection{
-		slice: ret,
+		slice: ret.Interface(),
 		err:   nil,
 	}
 }
--- flatten_test.go
+++ flatten_test.go
@@ -1,1 +1,22 @@
-package gollection
+package gollection_test
+
+import (
+	"testing"
+
+	"github.com/azihsoyn/gollection"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlatten(t *testing.T) {
+	assert := assert.New(t)
+	arr := [][]int{
+		{1, 2, 3},
+		{4, 5},
+		{6, 7, 8, 9, 10},
+	}
+	expect := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	res, err := gollection.New(arr).Flatten().Result()
+	assert.NoError(err)
+	assert.Equal(expect, res)
+}
