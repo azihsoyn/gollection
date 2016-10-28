b0ab27515d1898c5d22a86ac1e44fbf891b9802d
azihsoyn <azihsoyn@gmail.com>
2016-10-28 12:13:09 +0900

    modify filter

--- filter.go
+++ filter.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 )
 
-func (g *gollection) Filter(f func(v interface{}) bool) *gollection {
+func (g *gollection) Filter(f interface{}) *gollection {
 	if g.err != nil {
 		return &gollection{err: g.err}
 	}
@@ -18,11 +18,21 @@ func (g *gollection) Filter(f func(v interface{}) bool) *gollection {
 		}
 	}
 
-	ret := reflect.MakeSlice(sv.Type(), 0, sv.Len())
+	funcValue := reflect.ValueOf(f)
+	funcType := funcValue.Type()
+	if funcType.Kind() != reflect.Func || funcType.NumIn() != 1 || funcType.NumOut() != 1 || funcType.Out(0).Kind() != reflect.Bool {
+		return &gollection{
+			slice: nil,
+			err:   fmt.Errorf("gollection.Filter called with invalid func. required func(in <T>) bool but supplied %v", g.slice),
+		}
+	}
+
+	resultSliceType := reflect.SliceOf(funcType.In(0))
+	ret := reflect.MakeSlice(resultSliceType, 0, sv.Len())
 
 	for i := 0; i < sv.Len(); i++ {
 		v := sv.Index(i)
-		if f(v.Interface()) {
+		if funcValue.Call([]reflect.Value{v})[0].Interface().(bool) {
 			ret = reflect.Append(ret, sv.Index(i))
 		}
 	}
--- filter_test.go
+++ filter_test.go
@@ -12,11 +12,8 @@ func TestFilter(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	expect := []int{6, 7, 8, 9, 10}
 
-	res, err := gollection.New(arr).Filter(func(v interface{}) bool {
-		if n, ok := v.(int); ok && n > 5 {
-			return true
-		}
-		return false
+	res, err := gollection.New(arr).Filter(func(v int) bool {
+		return v > 5
 	}).Result()
 	assert.NoError(err)
 	assert.Equal(expect, res)
