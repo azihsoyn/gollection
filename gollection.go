e25212bdde9b8744bcad4bc1f444ad83a120f320
azihsoyn <azihsoyn@gmail.com>
2016-11-08 00:51:05 +0900

    support ResultAs

--- gollection.go
+++ gollection.go
@@ -1,5 +1,10 @@
 package gollection
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type gollection struct {
 	slice interface{}
 	val   interface{}
@@ -17,4 +22,18 @@ func (g *gollection) Result() (interface{}, error) {
 		return g.val, g.err
 	}
 	return g.slice, g.err
+}
+
+func (g *gollection) ResultAs(out interface{}) error {
+	if g.val != nil {
+		return g.err
+	}
+	iv := reflect.ValueOf(g.slice)
+	ov := reflect.ValueOf(out)
+	if ov.Kind() != reflect.Ptr || iv.Type() != ov.Elem().Type() {
+		return fmt.Errorf("gollection.ResultAs called with unexpected type %T, expected %s", g.slice, ov.Elem().Type())
+	}
+
+	ov.Elem().Set(iv)
+	return nil
 }
