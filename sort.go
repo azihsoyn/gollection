97c4b3b4d524a7c95292513540aca3cd0ebaa67b
azihsoyn <azihsoyn@gmail.com>
2016-11-10 23:04:31 +0900

    stream support sort

--- sort.go
+++ sort.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"sort"
+	"sync"
 
 	"go4.org/reflectutil"
 )
@@ -13,6 +14,14 @@ func (g *gollection) SortBy(f interface{}) *gollection {
 		return &gollection{err: g.err}
 	}
 
+	if g.ch != nil {
+		return g.sortByStream(f)
+	}
+
+	return g.sortBy(f)
+}
+
+func (g *gollection) sortBy(f interface{}) *gollection {
 	sv := reflect.ValueOf(g.slice)
 	if sv.Kind() != reflect.Slice {
 		return &gollection{
@@ -46,6 +55,69 @@ func (g *gollection) SortBy(f interface{}) *gollection {
 		slice: ret.Interface(),
 		err:   nil,
 	}
+}
+
+func (g *gollection) sortByStream(f interface{}) *gollection {
+	next := &gollection{
+		ch: make(chan interface{}),
+	}
+
+	funcValue := reflect.ValueOf(f)
+	funcType := funcValue.Type()
+	if funcType.Kind() != reflect.Func || funcType.NumIn() != 2 || funcType.NumOut() != 1 || funcType.Out(0).Kind() != reflect.Bool {
+		return &gollection{
+			slice: nil,
+			err:   fmt.Errorf("gollection.SortBy called with invalid func. required func(in1, in2 <T>) bool but supplied %v", g.slice),
+		}
+	}
+
+	var ret reflect.Value
+	var initialized bool
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for {
+			select {
+			case v, ok := <-g.ch:
+				if ok {
+					if !initialized {
+						ret = reflect.MakeSlice(reflect.SliceOf(reflect.ValueOf(v).Type()), 0, 0)
+						initialized = true
+					}
+					ret = reflect.Append(ret, reflect.ValueOf(v))
+				} else {
+					wg.Done()
+					return
+				}
+			default:
+				continue
+			}
+		}
+	}()
+	wg.Wait()
+
+	less := func(i, j int) bool {
+		return funcValue.Call([]reflect.Value{ret.Index(i), ret.Index(j)})[0].Interface().(bool)
+	}
+
+	sv := reflect.ValueOf(ret.Interface())
+
+	sort.Sort(&funcs{
+		length: sv.Len(),
+		less:   less,
+		swap:   reflectutil.Swapper(ret.Interface()),
+	})
+
+	go func() {
+		for i := 0; i < sv.Len(); i++ {
+			next.ch <- sv.Index(i).Interface()
+		}
+		close(next.ch)
+	}()
+	return next
+
+	return next
 }
 
 type funcs struct {
